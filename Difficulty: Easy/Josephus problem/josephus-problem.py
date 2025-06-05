eb57b7f379d324112ce79d89e11dd6ac694ac7e7
Raghu Valluru <108127007+Raghu1120@users.noreply.github.com>
2025-06-05 21:07:13 +0530

    Update josephus-problem.py
    Signed-off-by: Raghu Valluru <108127007+Raghu1120@users.noreply.github.com>

--- Difficulty: Easy/Josephus problem/josephus-problem.py
+++ Difficulty: Easy/Josephus problem/josephus-problem.py
@@ -3,3 +3,7 @@
 class Solution:
     def josephus(self,n,k):
         #Your code here
+        if n == 1:
+            return 1  
+        survivor = self.josephus(n - 1, k)
+        return (survivor + k - 1) % n + 1
