c6ed22629210d816f2ca6ccccd3814c34c6f4382
Raghu Valluru <108127007+Raghu1120@users.noreply.github.com>
2025-12-08 11:47:44 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (36.26%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -1,11 +1,11 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-
         words = s.split()
         if len(words) != len(pattern):
             return False
         hashp,hashs = {} , {}
-        for c1,c2 in zip(pattern,words):
+        for i in range(len(words)):
+            c1,c2 = pattern[i],words[i]
             if ((c1 in hashp and hashp[c1] != c2) or (c2 in hashs and hashs[c2] != c1)):
                 return False
             hashp[c1] = c2
